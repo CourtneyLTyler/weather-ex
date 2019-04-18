8b2eb4cc0cb04b8d49a445b5562d1f7fe560110a
Courtney Tyler <courtneytyler7@gmail.com>
2019-04-18 17:11:45 -0400

    successful stations data extraction, need to figure out secret key issue

--- Routes/StationRoutes.js
+++ Routes/StationRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const axios = require('axios');
 
 let key
 // unsafe - normally would not include a secret key within the code
@@ -25,11 +26,17 @@ fs.readFile("secrets.json", (err, data) => {
 
   
 
-router.get("/", (req, res) => {
-    res.json(key.APIKey);
-    
 
+
+router.get("/", (req, res) => {
+    // res.json(key.APIKey);
+    const header = `Authorization: Bearer ${key.APIKey}`;
+    axios.get('https://wx.wearebraid.com/stations', { headers: { header } }).then(ret => {
+        console.log(ret);
+        res.json(ret.data);
+    })
    }
 );
 
 module.exports = router;
+
