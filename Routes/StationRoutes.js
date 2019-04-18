bd9353d4950893f2d997de61702f56892d303d15
Courtney Tyler <courtneytyler7@gmail.com>
2019-04-18 17:13:43 -0400

    clean up

--- Routes/StationRoutes.js
+++ Routes/StationRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const fs = require("fs");
 const axios = require('axios');
 
-let key
+let key;
 // unsafe - normally would not include a secret key within the code
 fs.readFile("secrets.json", (err, data) => {
     if(err) throw err;
@@ -11,22 +11,6 @@ fs.readFile("secrets.json", (err, data) => {
     console.log(data.toString());
     key = JSON.parse(data.toString());
 });
-
-
-// curl "https://wx.wearebraid.com/{path}"
-//   -H "Authorization: Bearer your_secret_key"
-
-//   componentDidMount() {
-//     axios.get('https://wx.wearebraid.com/stations').then(res => {
-//       this.setState({ stations:
-//       res })
-//       console.log(this.state.stations)
-//     })
-//   }
-
-  
-
-
 
 router.get("/", (req, res) => {
     // res.json(key.APIKey);
